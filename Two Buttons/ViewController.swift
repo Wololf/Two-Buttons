f12d2e4a10d2fdf45ecf8bf2320465dac31de008
Mars <Mars@github.com>
2017-12-12 14:04:04 -0500

    Done

--- Two Buttons/ViewController.swift
+++ Two Buttons/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBAction func setTextButtonTapped(_ sender: UIButton) {
+      textLabel.text = textField.text
+    }
+    
+    @IBAction func clearTextButttonTapped(_ sender: UIButton) {
+        textField.text = ""
+        textLabel.text = ""
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
